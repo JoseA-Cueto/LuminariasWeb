f52fd3304363e9b9a170b637d992f7fe98fb1c27
JoseA-Cueto <joseacm2901@gmail.com>
2024-01-11 13:33:57 -0500

    Add logica para crearlo en Base Datos

---
+++ DataBaseInterface/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using LuminariasWeb.sln.Models;
+
+namespace LuminariasWeb.sln.DataBaseInterface
+{
+    public interface IUserRepository
+    {
+        Task AddUserAsync(User user);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using LuminariasWeb.sln.DataBaseInterface;
+using LuminariasWeb.sln.Models;
+
+namespace LuminariasWeb.sln.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public UserRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddUserAsync(User user)
+        {
+            await _dbContext.Set<User>().AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
