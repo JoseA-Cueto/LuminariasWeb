cf69770170c7f66f8c9e7c764e70781a010c16bc
JoseA-Cueto <joseacm2901@gmail.com>
2024-01-13 17:14:38 -0500

    Minor change

--- ViewModels/ServiceViewModel.cs
+++ ViewModels/ServiceViewModel.cs
@@ -4,7 +4,7 @@ namespace LuminariasWeb.sln.ViewModels
 {
     public class ServiceViewModel
     {
-        [Required(ErrorMessage = "El nombre es obligatorio")]
+      
         public string Name { get; set; }
 
         public decimal Price { get; set; }
