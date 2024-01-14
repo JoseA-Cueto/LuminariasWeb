5c573c5e844545373e5cf52992cd783c8c1903b5
JoseA-Cueto <joseacm2901@gmail.com>
2024-01-14 04:21:03 -0500

    arreglado problema 500

--- ViewModels/ProductViewModel.cs
+++ ViewModels/ProductViewModel.cs
@@ -5,12 +5,13 @@ namespace LuminariasWeb.sln.ViewModels
 {
     public class ProductViewModel
     {
-        
-        public string Name { get; set; }
-        public decimal Price { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
         public string? Description { get; set; }
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
+        [EnumDataType(typeof(ProductCategory))]
         public int? CategoryId { get; set; }
-        public string Category { get; set; }
+        public string? Category { get; set; }
     }
 }
